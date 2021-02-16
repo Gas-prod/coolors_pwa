4ef94218954627f003d7390b725b602a3497f921
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2021-02-16 19:36:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/VacationTimer/sw-toolbox.js");
+importScripts("/coolors_pwa/sw-toolbox.js");
 toolbox.precache([
     "/essai_pwa/icon/192x192.png", 
     "/essai_pwa/icon/512x512.png"
