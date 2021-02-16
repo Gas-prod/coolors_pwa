cd7720f6d50af00e931391f5734538ce752771c2
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2021-02-16 20:41:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("/coolors_pwa/sw-toolbox.js");
 toolbox.precache([
-    "/essai_pwa/icon/192x192.png", 
-    "/essai_pwa/icon/512x512.png"
+    "/coolors_pwa/icon/192x192.png", 
+    "/coolors_pwa/icon/512x512.png"
 ])
 
 toolbox.router.get("/*", toolbox.networkFirst, {
